1d9aca872b8cbea3d1509112d023004a134e4b2f
AliBaghdadi <alibaghdadi.developer@gmail.com>
2023-09-14 22:35:16 +0300

    adding fl_start onclicklistener

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.flStart.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Start btn has clicked!!", Toast.LENGTH_LONG).show()
+        }
     }
 }
