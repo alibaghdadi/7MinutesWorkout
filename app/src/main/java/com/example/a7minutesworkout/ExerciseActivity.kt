c22d680186e33b2a08c78467d40b678208c870c1
AlyBigdady <alybaghdady.91@gamil.com>
2023-09-16 23:34:37 +0300

    adding exercise timer

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -3,14 +3,19 @@ package com.example.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.Toast
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityExerciseBinding
+
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,33 +34,70 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        setupRestView()
+        setRestProgressBar()
+
     }
 
     private fun setupRestView() {
-        if (restTimer != null) {
-            restTimer?.cancel()
-            restProgress = 0
-        }
-        setRestProgressBar()
+
+        val tvConstraint = binding.tvTitle.layoutParams as ConstraintLayout.LayoutParams
+        tvConstraint.bottomToTop = binding.flRestProgressBar.id
+        binding.tvTitle.requestLayout()
+
+        binding.flExerciseProgressBar.visibility = View.GONE
+        binding.tvTitle.text = "Rest"
+        binding.flRestProgressBar.visibility = View.VISIBLE
+
+        resetRestTimers()
+        resetExerciseTimers()
+    }
+
+    private fun setupExerciseView() {
+
+        val tvConstraint = binding.tvTitle.layoutParams as ConstraintLayout.LayoutParams
+        tvConstraint.bottomToTop = binding.flExerciseProgressBar.id
+        binding.tvTitle.requestLayout()
+
+        binding.flRestProgressBar.visibility = View.GONE
+        binding.tvTitle.text = "Exercise Name"
+        binding.flExerciseProgressBar.visibility = View.VISIBLE
+
+        resetRestTimers()
+        resetExerciseTimers()
+
     }
 
     private fun setRestProgressBar() {
-        binding.progressBar.progress = restProgress
+        binding.restProgressBar.progress = restProgress
 
         restTimer = object : CountDownTimer(10000, 1000) {
             override fun onTick(p0: Long) {
                 restProgress++
-                binding.progressBar.progress = 10 - restProgress
-                binding.tvTimer.text = (10 - restProgress).toString()
+                binding.restProgressBar.progress = 10 - restProgress
+                binding.tvRestTimer.text = (10 - restProgress).toString()
             }
 
             override fun onFinish() {
-                Toast.makeText(
-                    this@ExerciseActivity,
-                    "Here now we will start the exercise",
-                    Toast.LENGTH_LONG
-                ).show()
+                setupExerciseView()
+                setExerciseProgressBar()
+            }
+
+        }.start()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding.exerciseProgressBar.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding.exerciseProgressBar.progress = 30 - exerciseProgress
+                binding.tvExerciseTimer.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                setupRestView()
+                setRestProgressBar()
             }
 
         }.start()
@@ -63,10 +105,21 @@ class ExerciseActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        resetRestTimers()
+        resetExerciseTimers()
+    }
 
+    private fun resetRestTimers() {
         if (restTimer != null) {
             restTimer?.cancel()
             restProgress = 0
+        }
+    }
+
+    private fun resetExerciseTimers() {
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
         }
     }
 
