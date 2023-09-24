b3e2dcbf0920fec9a907d4473a718e4fb28709cf
AliBaghdadi <alibaghdadi.developer@gmail.com>
2023-09-24 18:07:50 +0300

    create recycleView adapter

--- app/src/main/java/com/example/a7minutesworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseStatusAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.a7minutesworkout
 
-class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a7minutesworkout.databinding.ItemExerciseStatusBinding
+
+class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemExerciseStatusBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemExerciseStatusBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
