91f84776a55af2b45a1e870de78ca2da10cf28a3
AliBaghdadi <alibaghdadi.developer@gmail.com>
2023-09-24 20:12:14 +0300

    setup recycler adapter

--- app/src/main/java/com/example/a7minutesworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseStatusAdapter.kt
@@ -5,11 +5,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a7minutesworkout.databinding.ItemExerciseStatusBinding
 
-class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
+class ExerciseStatusAdapter(private val items: ArrayList<ExerciseModel>) :
     RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
 
     class ViewHolder(binding: ItemExerciseStatusBinding) : RecyclerView.ViewHolder(binding.root) {
-
+        val tvItem = binding.tvItem
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,11 +23,12 @@ class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val model: ExerciseModel = items[position]
+        holder.tvItem.text = model.getId().toString()
     }
 
 }
