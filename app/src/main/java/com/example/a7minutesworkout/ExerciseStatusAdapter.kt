528c452878e8d2947dc59d5f59f937deaed6dc71
AliBaghdadi <alibaghdadi.developer@gmail.com>
2023-09-27 19:17:49 +0300

    adding change circular status background

--- app/src/main/java/com/example/a7minutesworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseStatusAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.a7minutesworkout
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a7minutesworkout.databinding.ItemExerciseStatusBinding
 
@@ -29,6 +31,32 @@ class ExerciseStatusAdapter(private val items: ArrayList<ExerciseModel>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val model: ExerciseModel = items[position]
         holder.tvItem.text = model.getId().toString()
+
+        when {
+            model.getIsSelected() -> {
+                holder.tvItem.background = ContextCompat.getDrawable(
+                    holder.itemView.context,
+                    R.drawable.item_circular_thin_color_accent_border
+                )
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+
+            model.getIsCompleted() -> {
+                holder.tvItem.background = ContextCompat.getDrawable(
+                    holder.itemView.context,
+                    R.drawable.item_circular_color_accent_background
+                )
+                holder.tvItem.setTextColor(Color.WHITE)
+            }
+
+            else -> {
+                holder.tvItem.background = ContextCompat.getDrawable(
+                    holder.itemView.context,
+                    R.drawable.item_circular_gray_background
+                )
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+        }
     }
 
 }
