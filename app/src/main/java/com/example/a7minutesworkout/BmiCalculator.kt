aafa9659abfa92fd22aa2086cfd8e583ec1709b5
AliBaghdadi <alibaghdadi.developer@gmail.com>
2023-10-01 22:49:25 +0300

    adding binding to bmi class

--- app/src/main/java/com/example/a7minutesworkout/BmiCalculator.kt
+++ app/src/main/java/com/example/a7minutesworkout/BmiCalculator.kt
@@ -2,10 +2,15 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a7minutesworkout.databinding.ActivityBmiCalculatorBinding
 
 class BmiCalculator : AppCompatActivity() {
+    private lateinit var binding: ActivityBmiCalculatorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmi_calculator)
+        binding = ActivityBmiCalculatorBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
     }
 }
