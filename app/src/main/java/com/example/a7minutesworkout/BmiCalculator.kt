eab2bb6f3903cea75830e703ecb5956d5874ac95
AliBaghdadi <alibaghdadi.developer@gmail.com>
2023-10-03 17:36:18 +0300

    update some features in bmi calculator

--- app/src/main/java/com/example/a7minutesworkout/BmiCalculator.kt
+++ app/src/main/java/com/example/a7minutesworkout/BmiCalculator.kt
@@ -29,14 +29,16 @@ class BmiCalculator : AppCompatActivity() {
 
         binding.btnBMI.setOnClickListener {
 
-            val weight = binding.edMetricUnitWeight.text.toString().toFloat()
-            val height = binding.edMetricUnitHeight.text.toString().toFloat()
+            if (validateMetricUnits()) {
+                val weight = binding.edMetricUnitWeight.text.toString().toFloat()
+                val height = binding.edMetricUnitHeight.text.toString().toFloat()
 
-            val bmiResult = calculateBMI(weight, height)
-            val roundedBMIResult = Math.round(bmiResult * 100.0) / 100.0.toFloat()
-            val screenResult = bmiWeightRanges(roundedBMIResult)
-            
-            binding.tvBmi.text = screenResult
+                val bmiResult = calculateBMI(weight, height)
+                val roundedBMIResult = Math.round(bmiResult * 100.0) / 100.0.toFloat()
+                val screenResult = bmiWeightRanges(roundedBMIResult)
+
+                binding.tvBmi.text = screenResult
+            }
 
         }
 
@@ -47,13 +49,38 @@ class BmiCalculator : AppCompatActivity() {
         exitDialogFragment.show(supportFragmentManager, "MyDialog")
     }
 
-    private fun calculateBMI(weight: Float, height: Float): Float = weight / ((height / 100).pow(2))
+    private fun validateMetricUnits(): Boolean {
+        var isValid = true
+
+        binding.tilMetricUnitWeight.error = null
+        binding.tilMetricUnitHeight.error = null
+
+        if (binding.edMetricUnitWeight.text.toString().isEmpty()) {
+            isValid = false
+            binding.tilMetricUnitWeight.error = "Your weight is empty"
+        } else if (binding.edMetricUnitHeight.text.toString().isEmpty()) {
+            isValid = false
+            binding.tilMetricUnitHeight.error = "Your height is empty"
+        }
+
+        return isValid
+    }
+
+    private fun calculateBMI(weight: Float, height: Float): Float =
+        weight / ((height / 100).pow(2))
 
     private fun bmiWeightRanges(bmiResult: Float) = when {
-        bmiResult < 18.5F -> "YOUR BMI\n$bmiResult\nUnderweight"
+        bmiResult < 18.5F -> "YOUR BMI\n$bmiResult\nUnderweight\n" +
+                "Severely underweight!! Take care of yourself!! Eat more!"
+
         bmiResult in 18.5F..24.9F -> "YOUR BMI\n$bmiResult\nHealthy"
-        bmiResult in 25F..29.9F -> "YOUR BMI\n$bmiResult\noverweight"
-        bmiResult > 30F -> "YOUR BMI\n$bmiResult\nobese"
+
+        bmiResult in 25F..29.9F -> "YOUR BMI\n$bmiResult\nOverweight\n" +
+                "Oops! Take care of yourself!! Workout maybe!!"
+
+        bmiResult > 30F -> "YOUR BMI\n$bmiResult\nObese\n" +
+                "Oops! Take care of yourself!! Workout maybe!!"
+
         else -> "Error"
     }
 
